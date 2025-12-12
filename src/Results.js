f8ffe8a52dc1c585808f24fddbeb0b7581908695
SuzannaW-web <suzannawallis@gmail.com>
2025-12-12 22:20:38 +0000

    Added phonetics

--- src/Results.js
+++ src/Results.js
@@ -8,6 +8,8 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <h3 className="phonetics">/{props.results.phonetic}/</h3>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
