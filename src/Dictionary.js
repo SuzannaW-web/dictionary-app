af2003c48aa7b51e1bc438c36a4324e1889439fa
SuzannaW-web <suzannawallis@gmail.com>
2025-12-13 21:07:20 +0000

    Added photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,23 +3,56 @@ import axios from "axios";
 import "./Dictionary.css";
 import "./Results.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
+  let [error, setError] = useState(null);
 
-  let apiKey = "3o03ae1d8at693bf40d8fb4c10733055";
+  function handleDictionaryResponse(response) {
+    if (response.data && response.data.word) {
+      setResults(response.data);
+      setError(null);
+    } else {
+      setResults(null);
+      setError("Word not found");
+    }
+  }
 
-  function handleResponse(response) {
-    console.log(response.data);
-    setResults(response.data);
+  function handlePexelsResponse(response) {
+    if (response.data.photos.length > 0) {
+      setPhotos(response.data.photos);
+    } else {
+      setPhotos([]);
+    }
   }
 
   function search(event) {
-    event.preventDefault();
+    if (event) event.preventDefault();
+    if (!keyword) return;
 
-    let apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiURL).then(handleResponse);
+    setResults(null);
+    setPhotos(null);
+    setError(null);
+
+    const apiKey = "3o03ae1d8at693bf40d8fb4c10733055";
+    const apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios
+      .get(apiURL)
+      .then(handleDictionaryResponse)
+      .catch(() => {
+        setResults(null);
+        setError("Word not found");
+      });
+
+    const pexelsApiKey =
+      "MZyHmHAftzCsciC9FfGlbwAvsfbmtpO0RfStlyNgQBtVdDedUfOsGwDz";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=12`;
+    const headers = { Authorization: pexelsApiKey };
+
+    axios.get(pexelsApiUrl, { headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -33,6 +66,7 @@ export default function Dictionary() {
           type="search"
           className="SearchInput"
           placeholder="Search for a word"
+          value={keyword}
           onChange={handleKeywordChange}
         />
         <button type="submit" className="SearchButton">
@@ -40,7 +74,22 @@ export default function Dictionary() {
         </button>
       </form>
 
-      <Results results={results} />
+      {error && <p className="ErrorMessage">{error}</p>}
+
+      <div className="container">
+        <div className="row">
+          {results && (
+            <div className="col-12 col-md-6">
+              <Results results={results} key={keyword} />
+            </div>
+          )}
+          {photos && (
+            <div className="col-12 col-md-6">
+              <Photos photos={photos} key={keyword} />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
